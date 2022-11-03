5bd3e38b2e5f03766aa70b9fc0b84116c0fe38cf
yuji91 <yuji91.1227@gmail.com>
2022-11-03 10:28:00 +0900

    Add function count

--- react-like-button/src/App.tsx
+++ react-like-button/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,8 +13,15 @@ function App() {
 }
 
 function LikeButton() {
-  const count = 999;
-  return <span className="likeButton">♥ {count}</span>;
+  const [count, setCount] = useState(999);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <span className="likeButton" onClick={handleClick}>
+      ♥ {count}
+    </span>
+  );
 }
 
 export default App;
